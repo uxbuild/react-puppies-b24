81757b62d75e8ffdab41f3685bd1b2c750232900
locedu <locutus.edu@outlook.com>
2024-11-09 00:29:32 -0500

    completed: select featured puppy (updating app state)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-//import './App.css'
 import { puppyList } from "./data.js";
 import { useState } from "react";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
   console.log(puppies);
+
+  // why is this working, when is is this called? how are variables handled?
+  const featuredPup = puppies.find((pup)=>{ return pup.id === featPupId})
+
   return (
     <div className="App">
       {puppies.map((pup) => {
-        return <p key={pup.id}>{pup.name}</p>;
         
+        return (
+          <p
+            onClick={() => {
+              console.log("pup id:", pup.id);
+              setFeatPupId(pup.id);
+            }}
+            key={pup.id}
+            >
+            {pup.name}
+          </p>
+        )
       })}
+
+      {featPupId && <p>{featPupId}</p>}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
