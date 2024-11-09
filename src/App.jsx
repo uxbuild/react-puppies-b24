585e5a59a6562443d40ef669a853007004370454
locedu <locutus.edu@outlook.com>
2024-11-08 22:49:12 -0500

    add: first react pattern with data and key.

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,22 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
   // const [count, setCount] = useState(0)
 
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
   return (
-    <>
-      <div>
+    <div className="App">
+      {puppies.map((pup) => {
+        return <p key={pup.id}>{pup.name}</p>;
         
-        {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p> */}
-      </div>
-      
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      
-    </>
-  )
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
